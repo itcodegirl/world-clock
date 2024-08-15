9fea78b6089a6f48feeda8066fc4303a5cbd841c
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-08-15 15:32:36 -0500

    Update javascript.js
    When a new city is selected from the dropdown, the #cities div is cleared by setting citiesElement.innerHTML = '';.

--- src/javascript.js
+++ src/javascript.js
@@ -1,54 +1,49 @@
-function updateTime() {
-	// Chicago Time
-	let chicagoDateElement = document.querySelector('#chicago-date');
-	let chicagoTimeElement = document.querySelector('#chicago-time');
-	let chicagoTime = moment().tz("America/Chicago");
-	chicagoDateElement.innerHTML = chicagoTime.format("dddd, MMMM Do YYYY");
-	chicagoTimeElement.innerHTML = chicagoTime.format('h:mm:ss [<small>]A[</small>]');
+// Function to update the time for a given city
+function updateTime(cityTimeZone = "America/Chicago", dateElementId, timeElementId) {
+	let cityTime = moment().tz(cityTimeZone);
+	let dateElement = document.querySelector(`#${dateElementId}`);
+	let timeElement = document.querySelector(`#${timeElementId}`);
 
-	// New York Time
-	let newYorkDateElement = document.querySelector('#newyork-date');
-	let newYorkTimeElement = document.querySelector('#newyork-time');
-	let newYorkTime = moment().tz("America/New_York");
-	newYorkDateElement.innerHTML = newYorkTime.format("dddd, MMMM Do YYYY");
-	newYorkTimeElement.innerHTML = newYorkTime.format('h:mm:ss [<small>]A[</small>]');
+	if (dateElement && timeElement) {
+		dateElement.innerHTML = cityTime.format("dddd, MMMM Do YYYY");
+		timeElement.innerHTML = cityTime.format('h:mm:ss [<small>]A[</small>]');
+	}
 }
 
 // Update time for a selected city from the dropdown
 function updateCity(event) {
 	let cityTimeZone = event.target.value;
+
+	if (!cityTimeZone) return;  // If no valid city is selected, do nothing
+
 	let cityTime = moment().tz(cityTimeZone);
 	let cityName = cityTimeZone.split('/')[1].replace('_', ' ');
 
-	// Create a unique ID for the selected city (replacing slashes and underscores)
-	let cityId = cityTimeZone.toLowerCase().replace(/\//g, "-").replace(/_/g, "-");
+	// Clear the #cities div to display only the new city
+	let citiesElement = document.querySelector('#cities');
+	citiesElement.innerHTML = '';  // Clear the existing cities
 
-	// Check if the city already exists, otherwise create it
-	let existingCity = document.querySelector(`#${cityId}`);
-	if (!existingCity) {
-		let citiesElement = document.querySelector('#cities');
-		let newCityElement = `
-            <div class="row-city" id="${cityId}">
-                <div class="col city-name">
-                    <h2>${cityName}</h2>
-                    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-                    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-                </div>
+	// Create and display the selected city's time
+	let newCityElement = `
+        <div class="row-city">
+            <div class="col city-name">
+                <h2>${cityName}</h2>
+                <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+                <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
             </div>
-        `;
-		citiesElement.insertAdjacentHTML('beforeend', newCityElement);
-	} else {
-		// If the city already exists, just update the time
-		existingCity.querySelector('.date').innerHTML = cityTime.format("MMMM Do YYYY");
-		existingCity.querySelector('.time').innerHTML = `${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>`;
-	}
+        </div>
+    `;
+	citiesElement.insertAdjacentHTML('beforeend', newCityElement);
+
+	// Update the city's time every second
+	setInterval(() => {
+		let cityTime = moment().tz(cityTimeZone);
+		document.querySelector('.date').innerHTML = cityTime.format("MMMM Do YYYY");
+		document.querySelector('.time').innerHTML = `${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>`;
+	}, 1000);
 }
 
-// Call updateTime for initial load of Chicago and New York
-updateTime();
-setInterval(updateTime, 1000); // Update every second for both cities
-
-// Get the dropdown element and set event listener
+// Get the dropdown element and set event listener for new city selection
 let citiesSelectElement = document.querySelector('#timezone-dropdown');
 citiesSelectElement.addEventListener('change', updateCity);
 
